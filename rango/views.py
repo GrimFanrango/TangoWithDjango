6ba442e103e04b161032c04ba50880fa00311a10
Daniel Ashworth <2199252A@student.gla.ac.uk>
2017-02-07 20:05:28 +0000

    Final commit for the night

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says go eat a dick!")
+    return HttpResponse("Rango says go eat a dick! <br/> <a href='/rango/about/'>About</a>")
+def about(request):
+    return HttpResponse("memes")
