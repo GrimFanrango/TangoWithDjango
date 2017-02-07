b3f1fa9560f302f189b1883e6d74ca51f0518d3c
Daniel Ashworth <2199252A@student.gla.ac.uk>
2017-02-07 17:41:30 +0000

    changed app name to rango

--- TangoWithDjango/settings.py
+++ TangoWithDjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Tango_With_Django',
+    'rango',
 ]
 
 MIDDLEWARE_CLASSES = [
