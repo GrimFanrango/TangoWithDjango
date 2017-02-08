cadb91466444118e766f258aaf522d3e0835a549
Daniel Ashworth <2199252A@student.gla.ac.uk>
2017-02-08 18:07:24 +0000

    Chapter 3

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says go eat a dick! <br/> <a href='/rango/about/'>About</a>")
+    return HttpResponse("rango says hello! <br/> <a href='/rango/about/'>About</a>")
 def about(request):
-    return HttpResponse("memes")
+    return HttpResponse("rango says here is the about page <br/> <a href='/rango/'>Back to where you came from</a>")
