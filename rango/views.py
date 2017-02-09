d3b97bd2262b848bf5c62e196484b99e35bde5d5
Daniel Ashworth <2199252A@student.gla.ac.uk>
2017-02-09 18:48:51 +0000

    Pre exercise commit

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
 
 
 def index(request):
@@ -12,4 +12,32 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    #Create a context dictionary which we can pass
+    #to the template rendering engine
+    context_dict = {}
+
+    try:
+        #try to find a category with the slug with the given name?
+        category = Category.objects.get(slug=category_name_slug)
+
+        #Get all the associated pages.
+        pages = Page.objects.filter(category=category)
+
+        #add our results to the template context under name pages.
+        context_dict['pages'] = pages
+
+        #Also take the category object from the database
+        #and add it to the context dictionary.
+        #and use it to verify that the category exists
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        #If we didn't find the specified category we don't do anything
+        #The template will display a zero category error for us
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
 
