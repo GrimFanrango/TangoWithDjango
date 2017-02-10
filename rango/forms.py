a94ec869c3c5927bdfcf275c7fbd76f887f31355
Daniel Ashworth <2199252A@student.gla.ac.uk>
2017-02-10 01:56:01 +0000

    stuff is broken but zzzz

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
 
 
 class CategoryForm(forms.ModelForm):
@@ -44,3 +45,20 @@ class PageForm(forms.ModelForm):
         exclude = ('category',)
         # or specify the fields to include (i.e. not include the category field)
         # fields = ('title', 'url', 'views')
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+    website = forms.URLField(required=False)
+    picture = forms.ImageField(required=False)
+
+    class Meta:
+        model = UserProfile
+        fieds = ('website', 'picture')
+        exclude = ('user',)
--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.template.defaultfilters import slugify
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -33,3 +34,16 @@ class Page(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+    def __unicode__(self):
+        return self.user.username
